6cfe288a05c8e6672fdd79c4da65cbe9a865d0c2
Erdem Ceylan <erdemxceylan@gmail.com>
2022-03-04 15:40:51 +0300

    nutrient list table created

--- src/components/HomePage/components/ConsumedNutrientsTable.js
+++ src/components/HomePage/components/ConsumedNutrientsTable.js
@@ -5,8 +5,17 @@ import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
 import TotalValues from './TotalValues';
 import { consumedNutrientsActions } from '../../../global/redux/consumed-nutrients';
-import styles from './ConsumedNutrientsTable.module.css';
+import styles from '../../../App.module.css';
 import cn from 'classnames';
+
+const NAME = 'Name';
+const QUANTITY = 'Quantity';
+const CALORIES = 'Calories';
+const PROTEINS = 'Proteins';
+const NUTRIENT_NAME = 'name';
+const CONSUMED_QUANTITY = 'consumedQuantity';
+const CALORIES_TAKEN = 'caloriesTaken';
+const PROTEINS_TAKEN = 'proteinsTaken';
 
 function ConsumedNutrientsTable() {
 
@@ -14,30 +23,30 @@ function ConsumedNutrientsTable() {
    const dispatch = useDispatch();
 
    const consumedNutrientsTableColumns = [
-      { field: 'name', header: 'Name' },
-      { field: 'consumedQuantity', header: 'Quantity' },
-      { field: 'caloriesTaken', header: 'Calories' },
-      { field: 'proteinsTaken', header: 'Proteins' }
+      { field: NUTRIENT_NAME, header: NAME },
+      { field: CONSUMED_QUANTITY, header: QUANTITY },
+      { field: CALORIES_TAKEN, header: CALORIES },
+      { field: PROTEINS_TAKEN, header: PROTEINS }
    ];
 
    function labelField(rowData, field) {
       switch (field) {
-         case 'caloriesTaken':
-            return rowData.caloriesTaken.toFixed(2) + ' kcal';
-         case 'proteinsTaken':
-            return rowData.proteinsTaken.toFixed(2) + ' gram';
-         case 'consumedQuantity':
-            return `${rowData.consumedQuantity} ${rowData.unit}`;
-         case 'name':
+         case NUTRIENT_NAME:
             return rowData.name;
+         case CONSUMED_QUANTITY:
+            return `${rowData.consumedQuantity} ${rowData.unit}`;
+         case CALORIES_TAKEN:
+            return rowData.caloriesTaken.toFixed(2) + ' kcal';
+         case PROTEINS_TAKEN:
+            return rowData.proteinsTaken.toFixed(2) + ' gram';
          default:
             return;
       }
    }
 
-   function textEditor(options) {
+   function quantityEditor(options) {
       return <InputText
-         className={styles.input}
+         className={styles.quantity}
          type="number"
          min={0}
          value={options.value}
@@ -47,11 +56,7 @@ function ConsumedNutrientsTable() {
 
    function rowEditCompletionHandler(event) {
       let { index, newData, originalEvent } = event;
-
       const newValue = newData.consumedQuantity;
-
-      console.log(newValue);
-
       const rowData = consumedNutrients[index];
 
       if (newValue === rowData.consumedQuantity) {
@@ -77,51 +82,23 @@ function ConsumedNutrientsTable() {
       }
    }
 
-   // function cellEditCompletionHandler(event) {
-   //    let { rowData, newValue, originalEvent } = event;
-
-   //    if (newValue === rowData.consumedQuantity) {
-   //       return;
-   //    } else if (Number(newValue) > 0) {
-   //       const updatedNutrient = {
-   //          id: rowData.id,
-   //          name: rowData.name,
-   //          consumedQuantity: newValue,
-   //          caloriesTaken: rowData.caloriesTaken * +newValue / +rowData.consumedQuantity,
-   //          proteinsTaken: rowData.proteinsTaken * +newValue / +rowData.consumedQuantity
-   //       };
-   //       dispatch(consumedNutrientsActions.updateConsumedNutrient(updatedNutrient));
-   //    } else if (Number(newValue) === 0) {
-   //       const eraseData = {
-   //          id: rowData.id,
-   //          calories: rowData.caloriesTaken,
-   //          proteins: rowData.proteinsTaken
-   //       };
-   //       dispatch(consumedNutrientsActions.deleteConsumedNutrient(eraseData));
-   //    } else {
-   //       originalEvent.preventDefault();
-   //    }
-   // }
-
    return (
       <div className={cn("card p-fluid", styles.table)}>
          <DataTable
             value={consumedNutrients}
-            className="editable-cells-table"
             editMode="row"
-            dataKey="id"
             onRowEditComplete={rowEditCompletionHandler}
             responsiveLayout="scroll"
             footer={<TotalValues />}
          >
             {consumedNutrientsTableColumns.map(({ field, header }) => {
-               if (field === "consumedQuantity") {
+               if (field === CONSUMED_QUANTITY) {
                   return <Column
                      key={field}
                      field={field}
                      header={header}
                      body={rowData => labelField(rowData, field)}
-                     editor={options => textEditor(options)}
+                     editor={options => quantityEditor(options)}
                   />;
                } else {
                   return <Column
@@ -133,41 +110,11 @@ function ConsumedNutrientsTable() {
                }
             })}
             <Column
-               rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }}
+               rowEditor headerStyle={{ width: '8rem', minWidth: '8rem' }}
                bodyStyle={{ textAlign: 'center' }}
             />
          </DataTable>
       </div>
-
-      // <div className={cn("card p-fluid", styles.table)}>
-      //    <DataTable
-      //       value={consumedNutrients}
-      //       className="editable-cells-table"
-      //       editMode="cell"
-      //       responsiveLayout="scroll"
-      //       footer={<TotalValues />}
-      //    >
-      //       {consumedNutrientsTableColumns.map(({ field, header }) => {
-      //          if (field === "consumedQuantity") {
-      //             return <Column
-      //                key={field}
-      //                field={field}
-      //                header={header}
-      //                body={rowData => labelField(rowData, field)}
-      //                editor={options => textEditor(options)}
-      //                onCellEditComplete={cellEditCompletionHandler}
-      //             />;
-      //          } else {
-      //             return <Column
-      //                key={field}
-      //                field={field}
-      //                header={header}
-      //                body={rowData => labelField(rowData, field)}
-      //             />;
-      //          }
-      //       })}
-      //    </DataTable>
-      // </div>
    );
 }
 
--- src/components/NutrientList/NutrientList.js
+++ src/components/NutrientList/NutrientList.js
@@ -1,19 +1,93 @@
 import React, { useContext } from 'react';
 import DatabaseContext from '../../global/context/database-context';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
 import styles from '../../App.module.css';
+import cn from 'classnames';
+
+const NAME = 'Name';
+const UNIT = 'Unit';
+const CALORIES = 'Calories';
+const PROTEINS = 'Proteins';
+const NAME_FIELD = 'name';
+const UNIT_FIELD = 'unit';
+const CALORIES_FIELD = 'calories';
+const PROTEINS_FIELD = 'proteins';
 
 function NutrientList() {
    const databaseContext = useContext(DatabaseContext);
 
+   const nutrientListTableColumns = [
+      { field: NAME_FIELD, header: NAME },
+      { field: UNIT_FIELD, header: UNIT },
+      { field: CALORIES_FIELD, header: CALORIES },
+      { field: PROTEINS_FIELD, header: PROTEINS }
+   ];
+
+   function editor(options, field) {
+      if ([CALORIES_FIELD, PROTEINS_FIELD].includes(field)) {
+         return <InputText
+            className={styles.input}
+            type="number"
+            min={0}
+            value={options.value}
+            onChange={(e) => options.editorCallback(e.target.value)}
+         />;
+      }
+      return <InputText
+         className={styles.input}
+         value={options.value}
+         onChange={(e) => options.editorCallback(e.target.value)}
+      />;
+   }
+
+   function labelField(rowData, field) {
+      switch (field) {
+         case NAME_FIELD:
+            return rowData.name;
+         case UNIT_FIELD:
+            return `1 ${rowData.unit}`;
+         case CALORIES_FIELD:
+            return rowData.calories + ' kcal';
+         case PROTEINS_FIELD:
+            return rowData.proteins + ' gram';
+         default:
+            return;
+      }
+   }
+
+   function rowEditCompletionHandler(event) {
+      let { index, newData/*, originalEvent*/ } = event;
+      const selectedNutrient = databaseContext.nutrients[index];
+
+      console.log(newData, selectedNutrient);
+   }
+
    return (
       <div className={styles["page-layout"]}>
-         {databaseContext.nutrients.map(nutrient => (
-            <div key={nutrient.id}>
-               <h2>{nutrient.name}</h2>
-               <p>{nutrient.id}</p>
-               <p>{nutrient.unit} {nutrient.calories} {nutrient.proteins}</p>
-            </div>
-         ))}
+         <div className={cn("card p-fluid", styles.table)}>
+            <DataTable
+               value={databaseContext.nutrients}
+               editMode="row"
+               onRowEditComplete={rowEditCompletionHandler}
+               responsiveLayout="scroll"
+            >
+               {nutrientListTableColumns.map(({ field, header }) => {
+                  return <Column
+                     key={field}
+                     field={field}
+                     header={header}
+                     body={rowData => labelField(rowData, field)}
+                     editor={options => editor(options, field)}
+                  />;
+               })}
+               <Column
+                  rowEditor headerStyle={{ width: '8rem', minWidth: '8rem' }}
+                  bodyStyle={{ textAlign: 'center' }}
+               />
+            </DataTable>
+         </div>
       </div>
    );
 }
